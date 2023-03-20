dba07f88802b5bb57f5603f91ce236ea07c35ad8
Mohitkmr47G <120587670+Mohitkmr47G@users.noreply.github.com>
2023-03-20 20:56:29 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,80 @@
+// // the product of two numbers
+// public class Program
+// {
+// public static void main (String[]args)
+// {
+
+// int x=2;
+// int y=4;
+// System.out.println("The product of Two Number:"+ x*y);
+
+// }
+// }
+
+
+
+
+// // the ascii value of char U
+// public class Program2
+// {
+// public static void main(String[] args)
+// {
+// int x='U';
+// System.out.println("The ascii value of char U:" + x);
+// }
+// }
+
+
+
+
+// Rectangle of Area
+// public class Program2 
+// {
+// public static void main(String[] args)
+// {
+
+//     int length = 7;
+//     int width = 4;
+//     int Area = length*width;
+//     System.out.println("Length of rectangle :" + length);
+//     System.out.println("Width of rectangle :" + width);
+//     System.out.println("Area of rectangle :" + Area);
+
+// }
+// }
+
+
+
+// Cube of number
+// public class Program2
+// {
+// public static void main(String[]args)
+// {
+//     int x = 4;
+//     int cube = 4*4*4;
+//     System.out.println("The cube of num 4 is :" + cube);
+// }
+// }
+
+
+
+
+// Swapping two numbers
+// public class Program2
+// {
+// public static void main (String[]args)
+// {
+//     int num1=2;
+//     int num2=3;
+//     System.out.println("Before Swapping num1 = 2");
+//         System.out.println("Before Swapping num2 = 3");
+
+//     int temp;
+//     temp=num1;
+//     num1=num2;
+//     num2=temp;
+//     System.out.println("After Swapping the numbers num1 is:" + num1);
+//     System.out.println("After Swapping the numbers num2 is:" + num2);
+//     // System.out.println("After Swapping the numbers numbers is:" + temp);
+// }
+// }
